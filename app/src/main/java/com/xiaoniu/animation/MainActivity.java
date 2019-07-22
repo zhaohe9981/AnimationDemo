ed9746480aa5a583f242d794e92a94a99721fa17
zhaozohar <zh1010110@qq.com>
2019-07-22 10:41:42 +0800

    init project

---
+++ app/src/main/java/com/xiaoniu/animation/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xiaoniu.animation;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 主界面
+ * @author xiaoniu
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
